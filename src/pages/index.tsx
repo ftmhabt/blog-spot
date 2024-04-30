c2225bc30889f2ce2ddb398e9c86eb29cabbc4ce
ftmhabt <ftmh.a78@gmail.com>
2024-04-30 15:53:58 +0330

    Add search and posts to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,31 @@
-import Posts from "../components/posts"
+import Posts from "../components/posts";
+import Icon from "@mdi/react";
+import { mdiTextSearchVariant } from "@mdi/js";
 
 export default function Index() {
-
   return (
     <div>
-     <Hero/>
+      <Hero />
+      <Search />
+      <Posts />
     </div>
-  )
+  );
 }
 
-function Hero(){
-    return(
-        <div>
-            <div>Find</div>
-            <div>Stories</div>
-        </div>
-    )
+function Hero() {
+  return (
+    <div>
+      <div>Find</div>
+      <div>Stories</div>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <Icon path={mdiTextSearchVariant} size={1} />
+      <input type="text" />
+    </div>
+  );
 }
