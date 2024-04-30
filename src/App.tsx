fecf739a867590670d606330ed19a28761df822a
ftmhabt <ftmh.a78@gmail.com>
2024-04-30 15:42:12 +0330

    Move fetching to another file

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  "https://jqdvolylksnursrcbhll.supabase.co",
-  import.meta.env.VITE_ANON_KEY
-);
-
 function App() {
-  const [posts, setPosts] = useState<Array<object> | null>([]);
-
-  useEffect(() => {
-    getPosts();
-  }, []);
-
-  async function getPosts() {
-    try {
-      const { data } = await supabase.from("blogposts").select("*");
-      setPosts(data);
-      console.log(data);
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
-  }
+  
 
   return (
-    <ul>
-      {posts?.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
+    <div>
+
+    </div>
   );
 }
 
---
+++ src/components/fetch-posts.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    "https://jqdvolylksnursrcbhll.supabase.co",
+    import.meta.env.VITE_ANON_KEY
+  );
+
+export default function FetchPosts() {
+    
+  const [posts, setPosts] = useState<Array<object> | null>([]);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  async function getPosts() {
+    try {
+      const { data } = await supabase.from("blogposts").select("*");
+      setPosts(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  }
+
+  return (
+    <ul>
+      {posts?.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
