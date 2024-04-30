b7eac663702920c2200a417a3782a59bdff29b8c
ftmhabt <ftmh.a78@gmail.com>
2024-04-30 15:37:59 +0330

    Fix anon key

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://jqdvolylksnursrcbhll.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxZHZvbHlsa3NudXJzcmNiaGxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0MjA1ODksImV4cCI6MjAyOTk5NjU4OX0.9HupmF5VSvone4YrG1_UT7IffX9cYS3I-LVet7xaaNw"
+  import.meta.env.VITE_ANON_KEY
 );
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
 
   async function getPosts() {
     try {
-      const { data } = await supabase.from("blogposts").select('*');
+      const { data } = await supabase.from("blogposts").select("*");
       setPosts(data);
       console.log(data);
     } catch (error) {
