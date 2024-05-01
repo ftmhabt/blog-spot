3af22d2516560dc58f0e058e80f60a1a02591621
ftmhabt <ftmh.a78@gmail.com>
2024-05-01 21:01:28 +0330

    Improve posts section styles

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -3,6 +3,8 @@ import FetchPosts from "./fetch-posts";
 import { BlogContext } from "../context/context";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import Icon from '@mdi/react';
+import { mdiArrowRight } from '@mdi/js';
 
 export default function Posts() {
   FetchPosts();
@@ -21,11 +23,13 @@ export default function Posts() {
   return (
     <div ref={container}>
       {blog.isLoading && <div>{"Loading..."}</div>}
-      <ul>
+      <ul className="flex flex-col gap-[1rem] text-[#FAFAFA]">
         {blog &&
           blog.posts?.map((post) => (
-            <li className="card opacity-0" key={post.id}>
-              {post.title}
+            <li className="card opacity-0 flex flex-col gap-[1rem] bg-[#2E2E2E] p-[1rem] rounded-3xl" key={post.id}>
+                <div className="text-3xl font-bold">{post.title}</div>
+                <div className="font-thin">{post.snippet}</div>
+                <div className="font-thin flex">More <Icon className="self-center" path={mdiArrowRight} size={.7} /></div>
             </li>
           ))}
       </ul>
