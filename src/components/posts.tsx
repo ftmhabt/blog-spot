3c3e6f0341e79dd2ce81b9b1b6255f9922ad1465
ftmhabt <ftmh.a78@gmail.com>
2024-05-01 19:22:14 +0330

    Add & try gsap

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,15 +1,33 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import FetchPosts from "./fetch-posts";
 import { BlogContext } from "../context/context";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Posts() {
   FetchPosts();
   const blog = useContext(BlogContext);
 
+  gsap.registerPlugin(useGSAP);
+
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      gsap.to(".box", { stagger: 1, x: 30, opacity: 1 });
+    },
+    { scope: container }
+  );
   return (
-    <div>
+    <div ref={container}>
+      <div>{blog.isLoading && "Loading..."}</div>
       <ul>
-        {blog && blog.posts?.map((post) => <li key={post.id}>{post.title}</li>)}
+        {blog &&
+          blog.posts?.map((post) => (
+            <li className="box opacity-0" key={post.id}>
+              {post.title}
+            </li>
+          ))}
       </ul>
     </div>
   );
