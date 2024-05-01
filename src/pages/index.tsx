ea2d5b415ecf57104efe0ad25538908629c31f6c
ftmhabt <ftmh.a78@gmail.com>
2024-05-01 21:10:56 +0330

    Improve hero and search style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { mdiTextSearchVariant } from "@mdi/js";
 
 export default function Index() {
   return (
-    <div>
+    <div className="text-[#2E2E2E]">
       <Hero />
       <Search />
       <Posts />
@@ -14,18 +14,18 @@ export default function Index() {
 
 function Hero() {
   return (
-    <div>
+    <div className="text-5xl p-[1.1rem] font-bold">
       <div>Find</div>
-      <div>Stories</div>
+      <div className="pl-[3rem]">stories!</div>
     </div>
   );
 }
 
 function Search() {
   return (
-    <div>
-      <Icon path={mdiTextSearchVariant} size={1} />
-      <input type="text" />
+    <div className="bg-[#F8F8F8] p-[1rem] flex gap-2 mb-[1rem] rounded-3xl">
+      <Icon className="text-[#9CA3AF]" path={mdiTextSearchVariant} size={1} />
+      <input className="w-full bg-[#F8F8F8]" type="text" placeholder="Search" />
     </div>
   );
 }
